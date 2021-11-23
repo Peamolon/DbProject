24fef3d8287a1ac32f702cae04e796639805840f
Sergio Peña <sergioeduardo2000@gmail.com>
2021-11-23 11:19:01 -0500

    Creating the first table

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -40,7 +40,7 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to root_path, notice: "Employee was successfully updated." }
+        format.html { redirect_to @employee, notice: "Employee was successfully updated." }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit, status: :unprocessable_entity }
